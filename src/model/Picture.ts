f891f6b2accbd6cd007fba277e9c67b41b689f3f
xome <johannes@einmayer.de>
2024-01-10 14:55:05 +0100

    Create Model

--- src/model/Picture.ts
+++ src/model/Picture.ts
@@ -1,37 +1,39 @@
-class Picture implements ChapterRecord {
+namespace dmHelper {
+  export class Picture implements Record {
 
-  private _base64: string;
-  private _fileFormat: string;
-  private _isShareableWithGroup: boolean;
+    private _base64: string;
+    private _fileFormat: string;
+    private _isShareableWithGroup: boolean;
 
 
-  constructor(base64: string, fileFormat: string, isShareableWithGroup: boolean) {
-    this._base64 = base64;
-    this._fileFormat = fileFormat;
-    this._isShareableWithGroup = isShareableWithGroup;
-  }
+    constructor(base64: string, fileFormat: string, isShareableWithGroup: boolean) {
+      this._base64 = base64;
+      this._fileFormat = fileFormat;
+      this._isShareableWithGroup = isShareableWithGroup;
+    }
 
-  get base64(): string {
-    return this._base64;
-  }
+    get base64(): string {
+      return this._base64;
+    }
 
-  set base64(value: string) {
-    this._base64 = value;
-  }
+    set base64(value: string) {
+      this._base64 = value;
+    }
 
-  get fileFormat(): string {
-    return this._fileFormat;
-  }
+    get fileFormat(): string {
+      return this._fileFormat;
+    }
 
-  set fileFormat(value: string) {
-    this._fileFormat = value;
-  }
+    set fileFormat(value: string) {
+      this._fileFormat = value;
+    }
 
-  get isShareableWithGroup(): boolean {
-    return this._isShareableWithGroup;
-  }
+    get isShareableWithGroup(): boolean {
+      return this._isShareableWithGroup;
+    }
 
-  set isShareableWithGroup(value: boolean) {
-    this._isShareableWithGroup = value;
+    set isShareableWithGroup(value: boolean) {
+      this._isShareableWithGroup = value;
+    }
   }
 }
