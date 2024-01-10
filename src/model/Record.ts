61d4d325e14d14bbe3c11fc6faf491eb8a715b83
xome <johannes@einmayer.de>
2024-01-10 14:57:07 +0100

    Create Model

---
+++ src/model/Record.ts
@@ -0,0 +1,5 @@
+namespace dmHelper {
+  export interface Record {
+
+  }
+}
---
+++ src/model/Text.ts
@@ -0,0 +1,17 @@
+namespace dmHelper {
+  export class Text implements Record {
+    private _text: string;
+
+    constructor(text: string) {
+      this._text = text;
+    }
+
+    get text(): string {
+      return this._text;
+    }
+
+    set text(value: string) {
+      this._text = value;
+    }
+  }
+}
