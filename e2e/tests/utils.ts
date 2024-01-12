5d4fcf4a8ea50948bab7b7b441b0127f673803d1
xome <johannes@einmayer.de>
2024-01-12 12:25:08 +0100

    setup e2e with testcontainers with real backend

--- e2e/tests/utils.ts
+++ e2e/tests/utils.ts
@@ -1,9 +1,32 @@
-
 import * as puppeteer from 'puppeteer';
+import {DockerComposeEnvironment} from "testcontainers";
+import {
+  StartedDockerComposeEnvironment
+} from "testcontainers/build/docker-compose-environment/started-docker-compose-environment";
 
 const baseUrl = process.env['baseUrl'] ?? 'http://localhost:4200/';
 let browser: puppeteer.Browser;
 let page: puppeteer.Page;
+
+let container: StartedDockerComposeEnvironment | undefined;
+
+if (process.env['DOCKER_COMPOSE_PATH'] == undefined) {
+  throw new Error("Path of docker-compose file for backend testcontainer is undefined!");
+}
+const composePath: string = process.env['DOCKER_COMPOSE_PATH'];
+
+if (process.env['DOCKER_COMPOSE_FILE'] == undefined) {
+  throw new Error("docker-compose file for backend testcontainer is undefined!");
+}
+const composeFile: string = process.env['DOCKER_COMPOSE_FILE'];
+
+
+if (process.env['DOCKER_ENV_FILE'] == undefined) {
+  throw new Error(".env file for backend testcontainer is undefined!");
+}
+const dockerEnvFile: string = process.env['DOCKER_ENV_FILE'];
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
 
 export function setupBrowserHooks(path = ''): void {
 
@@ -11,6 +34,10 @@ export function setupBrowserHooks(path = ''): void {
     browser = await puppeteer.launch({
       headless: 'new'
     });
+    container = await
+      new DockerComposeEnvironment(composePath, composeFile)
+        .withEnvironmentFile(dockerEnvFile)
+        .up();
   });
 
 
@@ -26,6 +53,8 @@ export function setupBrowserHooks(path = ''): void {
 
   afterAll(async () => {
     await browser.close();
+    if (container != undefined)
+      await container.down();
   });
 
 }
